0985273208b687f7dd64abb2959d97635f65f794
justinchuntingho <zjustin334@gmail.com>
2020-07-16 00:30:14 +0100

    Update tokens_cantonese.R

--- R/tokens_cantonese.R
+++ R/tokens_cantonese.R
@@ -16,10 +16,8 @@ tokens_cantonese <- function(corpus){
   tokenizer <- jiebaR::worker(type = "mix", dict = file.path(sonpath,"dict", "dict.txt.big"),
                               user = file.path(sonpath,"dict", "SonGotSTI.txt"))
   raw_texts <- quanteda::texts(corpus)
-  tokenised_texts_list <- purrr::map(raw_texts, jiebaR::segment, tokenizer)
-  tokenised_texts <- purrr::map(tokenised_texts_list, paste, collapse = " ")
-  quanteda::texts(corpus) <- tokenised_texts
-  return(quanteda::tokens(corpus, what = "fastestword"))
+  tokenised_texts <- purrr::map(raw_texts, jiebaR::segment, tokenizer)
+  return(quanteda::as.tokens(tokenised_texts))
 }
 
 
